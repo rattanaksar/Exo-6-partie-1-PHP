2b990c150d750ddbaa241efa4080064112f02466
rattanaksar <81554110+rattanaksar@users.noreply.github.com>
2021-06-09 11:11:17 +0200

    Exo 6 PHP

---
+++ Exo 6.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo 6 : Les variables PHP</title>
+</head>
+<body>
+    <h1>Exo 6 Partie : Les variables PHP</h1>
+    <?php
+        $name= "Rattanak";
+        echo "Bonjour $name, comment vas-tu ?";
+    ?>
+</body>
+</html>
